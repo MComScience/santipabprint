ddee330f5a9dde9a382dc52efe0688afe17dd2d1
MComScience <m-com3@hotmail.com>
2018-12-19 10:01:27 +0700

    bug fixed

--- common/themes/kidz/modules/yii2-user/models/User.php
+++ common/themes/kidz/modules/yii2-user/models/User.php
@@ -1,9 +1,42 @@
 <?php
 namespace kidz\user\models;
 
+use Yii;
 use dektrium\user\models\User as BaseUser;
+use dektrium\user\helpers\Password;
 
 class User extends BaseUser
 {
+	public function create()
+    {
+        if ($this->getIsNewRecord() == false) {
+            throw new \RuntimeException('Calling "' . __CLASS__ . '::' . __METHOD__ . '" on existing user');
+        }
 
+        $transaction = $this->getDb()->beginTransaction();
+
+        try {
+            $this->password = $this->password == null ? Password::generate(8) : $this->password;
+
+            $this->trigger(self::BEFORE_CREATE);
+
+            if (!$this->save()) {
+                $transaction->rollBack();
+                return false;
+            }
+
+            $this->confirm();
+
+            //$this->mailer->sendWelcomeMessage($this, null, true);
+            $this->trigger(self::AFTER_CREATE);
+
+            $transaction->commit();
+
+            return true;
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            \Yii::warning($e->getMessage());
+            throw $e;
+        }
+    }
 }
