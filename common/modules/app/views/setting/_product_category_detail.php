16c1a69693b3bd0383643f2e85552bb0966b7104
Pun-ratchada <pun.bxxx@gmail.com>
2019-07-22 16:43:09 +0700

    # pun //ประเภทสินค้า

---
+++ common/modules/app/views/setting/_product_category_detail.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use adminlte\widgets\GridView;
+use adminlte\helpers\Html;
+use kartik\icons\Icon;
+use yii\helpers\Url;
+?>
+<div class="kv-detail-content">
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <?=
+            GridView::widget([
+                'dataProvider' => $dataProvider,
+                'tableOptions' => [
+                    'class' => 'table-bordered table-condensed table-hover kv-table'
+                ],
+                'beforeHeader' => [
+                    [
+                        'columns' => [
+                            ['content' => 'ประเภทสินค้า', 'options' => ['class' => 'success text-center', 'colspan' => 4]]
+                        ]
+                    ]
+                ],
+                'layout' => '{items}',
+                'columns' => [
+                    [
+                        'class' => '\kartik\grid\SerialColumn',
+                         'width' => '20%',
+                    ],
+                    [
+                        'attribute' => 'package_type_name',
+                        'label' => 'ชื่อประเภทสินค้า',
+                        'hAlign' => 'rigth',
+                        'vAlign' => 'middle',
+                        'headerOptions' => [
+                            'style' => 'text-align:center;',
+                        ],
+                        'format' => 'raw',
+                       
+                    ]
+                    
+                ]
+            ])
+            ?>
+        </div>
+    </div>
+</div>
+
