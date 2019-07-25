c4f83143142c4f06a1f415dab2cf8fc8dcb9fdb3
MComScience <m-com3@hotmail.com>
2019-07-25 19:18:31 +0700

    fix bug load image url

--- common/modules/app/controllers/ApiController.php
+++ common/modules/app/controllers/ApiController.php
@@ -47,7 +47,7 @@ class ApiController extends \yii\web\Controller
             $itemCatagorys[] = [
                 'product_category_id' => $catagory['product_category_id'],
                 'product_category_name' => $catagory['product_category_name'],
-                'image_url' => Url::base(true) . $catagory->getImageUrl(),
+                'image_url' => $catagory->getImageUrl(),
             ];
         }
         return $itemCatagorys;
