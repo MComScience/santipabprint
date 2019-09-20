a8f7fca59511c89073f0ec0cba983ef110733441
MComScience <m-com3@hotmail.com>
2019-09-20 20:47:02 +0700

    test line

--- common/modules/app/views/product/_form_download.php
+++ common/modules/app/views/product/_form_download.php
@@ -65,97 +65,122 @@ use kartik\icons\Icon;
     <div class="row">
         <div class="col-sm-12 text-right">
             <?= Html::button(Icon::show('close') . 'ยกเลิก', ['class' => 'btn btn-default', 'data-dismiss' => 'modal']) ?>
-            <?= Html::submitButton(Icon::show('print') . 'ใบเสนอราคา', ['class' => 'btn btn-info', 'data-loading-text' => 'Loading...']) ?>
+            <?= Html::submitButton(Icon::show('print') . 'ดาวน์โหลดใบเสนอราคา', ['class' => 'btn btn-info', 'data-loading-text' => 'Loading...']) ?>
         </div>
     </div>
 <?php ActiveForm::end(); ?>
-
+<div id="form-download">
+    {{ liffData }}
+</div>
 <?php
 $this->registerJsFile(
     'https://d.line-scdn.net/liff/1.0/sdk.js',
     ['depends' => [\yii\web\JqueryAsset::className()]]
 );
-$this->registerJsFile('@web/js/liff-starter.js',
+/* $this->registerJsFile('@web/js/liff-starter.js',
         ['depends' => [\yii\web\JqueryAsset::className()]]
-);
+); */
 $this->registerJs(<<<JS
-liff.sendMessages([{
-            type: 'text',
-            text: "You've successfully sent a message! Hooray!"
-        }, {
-            type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
-        }]).then(function () {
-            window.alert("Message sent");
-        }).catch(function (error) {
-            window.alert("Error sending message: " + error);
-        });
-$('#ajaxCrudModal .modal-footer').hide();
-// if($('.list-group').find('a.list-group-item.active').length === 0){
-//     Swal({
-//         type: 'warning',
-//         title: 'Oops!',
-//         text: 'กรุณาเลือกจำนวนที่ต้องการพิมพ์',
-//     });
-//     $('#ajaxCrudModal').modal('hide');
-// }
-var \$form = $('#form-download');
-var \$formQuo = $('#form-quotation');
-\$form.on('beforeSubmit', function() {
-    var dataObj = {}, dataQO = {};
-    var \$items = $('.list-group').find('.list-group-item.active');
-    var qty = 0, final_price = 0;
-    \$items.each(function( i,v ) {
-        qty = $(this).data('qty');
-        final_price = $(this).data('final_price');
-    });
-    \$form.serializeArray().map(function (x) {
-        dataObj[x.name] = x.value;
-    });
-    /* \$formQuo.serializeArray().map(function (x) {
-        if(x.name === 'TblQuotationDetail[cust_quantity]'){
-            dataQO[x.name] = qty;
-        } else {
-            dataQO[x.name] = x.value;
-        }
-    }); */
-    if (localStorage.getItem("formData")) {
-        const formData = JSON.parse(localStorage.getItem("formData"));
-        dataQO = formData['{$modelDetail->product_id}'];
-    }
-    var \$btn = $('#form-download button[type="submit"]').button('loading');
-    $.ajax({
-        url: \$form.attr('action'),
-        type: \$form.attr('method'),
-        data: $.extend(dataObj, dataQO),
-        dataType:'JSON',
-        success: function (response) {
-            // Implement successful
-            \$btn.button('reset');
-            if (response.success) {
-                localStorage.removeItem('formData');
-                $('#ajaxCrudModal').modal('hide');
-                
-                // window.location.href = response.url;
-            } else {
-                Swal({
-                    type: 'error',
-                    title: 'Oops!',
-                    text: 'เกิดข้อผิดพลาด!',
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var myApp = new Vue({
+        el: '#form-download',
+        data: {
+            liffData: null
+        },
+        mounted() {
+            this.initializeApp()
+        },
+        methods: {
+            initializeApp(){
+                var _this = this
+                liff.init(function (data) {
+                    _this.liffData = data
+                });
+            },
+            sendMessages(messages) {
+                liff.sendMessages([{
+                    type: 'text',
+                    text: "You've successfully sent a message! Hooray!"
+                }, {
+                    type: 'sticker',
+                    packageId: '2',
+                    stickerId: '144'
+                }]).then(function () {
+                    window.alert("Message sent");
+                }).catch(function (error) {
+                    window.alert("Error sending message: " + error);
                 });
             }
         },
-        error: function (jqXHR, textStatus, errorThrown) {
-            \$btn.button('reset');
-            Swal({
-                type: 'error',
-                title: textStatus,
-                text: errorThrown,
-            });
+    })
+
+    $('#ajaxCrudModal .modal-footer').hide();
+    // if($('.list-group').find('a.list-group-item.active').length === 0){
+    //     Swal({
+    //         type: 'warning',
+    //         title: 'Oops!',
+    //         text: 'กรุณาเลือกจำนวนที่ต้องการพิมพ์',
+    //     });
+    //     $('#ajaxCrudModal').modal('hide');
+    // }
+    var \$form = $('#form-download');
+    var \$formQuo = $('#form-quotation');
+    \$form.on('beforeSubmit', function() {
+        var dataObj = {}, dataQO = {};
+        var \$items = $('.list-group').find('.list-group-item.active');
+        var qty = 0, final_price = 0;
+        \$items.each(function( i,v ) {
+            qty = $(this).data('qty');
+            final_price = $(this).data('final_price');
+        });
+        \$form.serializeArray().map(function (x) {
+            dataObj[x.name] = x.value;
+        });
+        /* \$formQuo.serializeArray().map(function (x) {
+            if(x.name === 'TblQuotationDetail[cust_quantity]'){
+                dataQO[x.name] = qty;
+            } else {
+                dataQO[x.name] = x.value;
+            }
+        }); */
+        if (localStorage.getItem("formData")) {
+            const formData = JSON.parse(localStorage.getItem("formData"));
+            dataQO = formData['{$modelDetail->product_id}'];
         }
+        var \$btn = $('#form-download button[type="submit"]').button('loading');
+        $.ajax({
+            url: \$form.attr('action'),
+            type: \$form.attr('method'),
+            data: $.extend(dataObj, dataQO),
+            dataType:'JSON',
+            success: function (response) {
+                // Implement successful
+                \$btn.button('reset');
+                if (response.success) {
+                    // localStorage.removeItem('formData');
+                    $('#ajaxCrudModal').modal('hide');
+                    myApp.sendMessages({})
+                    // window.location.href = response.url;
+                } else {
+                    Swal({
+                        type: 'error',
+                        title: 'Oops!',
+                        text: 'เกิดข้อผิดพลาด!',
+                    });
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                \$btn.button('reset');
+                Swal({
+                    type: 'error',
+                    title: textStatus,
+                    text: errorThrown,
+                });
+            }
+        });
+        return false; // prevent default submit
     });
-    return false; // prevent default submit
 });
 JS
 );
