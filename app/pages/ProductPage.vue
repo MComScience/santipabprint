c7ac3c0d9d1ed9961fdcf6b94e8fb5eb0b610dd4
Pun-ratchada <pun.bxxx@gmail.com>
2019-10-22 16:41:16 +0700

    #pun //แก้ไข detail

--- app/pages/ProductPage.vue
+++ app/pages/ProductPage.vue
@@ -1208,7 +1208,7 @@
                         <p v-if="attr === 'book_inner_paper'" class="info-label text-center kt-font-brand">
                           ข้อมูลเนื้อใน
                         </p>
-                        <p v-if="attr === 'book_inner_color'" class="info-label text-left kt-font-brand">
+                        <p v-if="attr === 'book_inner_paper'" class="info-label text-left kt-font-brand">
                           พิมพ์สี
                         </p>
                         <p v-if="attr === 'book_inner_paper_without_color'" class="info-label text-left kt-font-brand">
